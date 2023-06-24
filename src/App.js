adde71a1930119959c7f48134c9db6dbf3b89a6b
Mahesh Gowrabathina <maheshcodeinn@gmail.com>
2023-06-24 20:10:39 +0530

    redux basic app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import './App.css';
+import { decrementCount, incrementCount } from './store/store';
 
 function App() {
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch(incrementCount(2));
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrementCount(3));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={handleIncrement}>Increment</button>
+      <h3>{count}</h3>
+      <button onClick={handleDecrement}>Decrement</button>
     </div>
   );
 }
