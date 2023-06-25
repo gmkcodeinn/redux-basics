57106d36851f927cf443fd0e6c0010b5826e5f81
gmkcodeinn <maheshcodeinn@gmail.com>
2023-06-25 11:33:29 +0530

    added multiple reducers and middlewares

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import './App.css';
-import { decrementCount, incrementCount } from './store/store';
+import {
+  decrementCount,
+  incrementCount,
+  searchText,
+  submitClicked,
+} from './store/store';
 
 function App() {
-  const count = useSelector((state) => state.count);
+  const count = useSelector((state) => state.counter.count);
+  const searchData = useSelector((state) => state.search);
+
   const dispatch = useDispatch();
 
   const handleIncrement = () => {
@@ -15,11 +22,30 @@ function App() {
     dispatch(decrementCount(3));
   };
 
+  const handleInputChange = (e) => {
+    dispatch(searchText(e.target.value));
+  };
+
+  const handleSubmitClicked = () => {
+    dispatch(submitClicked());
+  };
+
   return (
     <div className='App'>
       <button onClick={handleIncrement}>Increment</button>
       <h3>{count}</h3>
       <button onClick={handleDecrement}>Decrement</button>
+      <br />
+      <br />
+      <div>
+        <input
+          type='text'
+          value={searchData.searchText}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleSubmitClicked}>Submit</button>
+      </div>
+      {searchData.isSubmitClicked && <h1>{searchData.searchText}</h1>}
     </div>
   );
 }
